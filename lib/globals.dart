7ce2b14c64adae7da3d16a6d42ae4e0ea919d713
Mudit-iitropar <149925616+Mudit-iitropar@users.noreply.github.com>
2025-04-26 14:57:11 +0530

    Update globals.dart

--- lib/globals.dart
+++ lib/globals.dart
@@ -4,9 +4,9 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 class Globals {
   static String? currentStation;
   static String? from_station;
+  static String? to_station;
   static final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
   static final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 }
-
 
